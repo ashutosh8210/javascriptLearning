cd8f62336131384fb48cc7e9b460d7cbed2f38cd
Ashutosh kumar <rajashutosh556@gmail.com>
2023-12-16 13:24:12 +0530

    dtatypes

---
+++ 01_basics/01_variables/datatype.js
@@ -0,0 +1,25 @@
+"use strict";  //treat this code as newer version
+
+// alert(2+5)   Not used as this in Node js. can be used in browser
+
+let name="Ashutosh"
+let age="23"
+let isQualified=true
+
+/*
+number
+bigint
+string -> ""
+null ---> standalone value
+undefined
+symbol
+
+Object
+*/
+
+console.log(typeof name);
+
+name=34
+console.log(name + " "+typeof name )
+console.log(typeof null)       //object
+console.log(typeof undefined);  //undefined
