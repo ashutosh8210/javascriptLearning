c01039371c8af30c51ab951feb4bcae3d764bad3
Ashutosh kumar <rajashutosh556@gmail.com>
2023-12-17 01:02:31 +0530

    conversion of datatypes

---
+++ 01_basics/01_variables/03_conversionDatatype.js
@@ -0,0 +1,31 @@
+let id=null
+
+// console.log(typeof(id));
+// console.log(typeof id);
+
+let idInNumber=Number(id)
+
+// console.log(typeof(idInNumber));
+// console.log(idInNumber);
+
+// "33" => 33
+// "33abc" =>NaN
+// undefined => NaN
+// null => 0
+
+
+// converting  to boolean
+
+let someVariable=-4
+
+let variableInBoolean=Boolean(someVariable)
+
+console.log(variableInBoolean)
+
+// 0 => false
+// 1 or any number => true
+// "" => false
+// "ashu" => true
+
+
+
