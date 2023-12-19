c66b2f741df56b0af86e3fc8c9990277979227b9
Ashutosh kumar <rajashutosh556@gmail.com>
2023-12-20 00:52:17 +0530

    more strings

--- 01_basics/01_variables/04_string.js
+++ 01_basics/01_variables/04_string.js
@@ -7,7 +7,20 @@ console.log(`My name is ${name} and my age is ${23}`);
 
 const city=new String("bangalore")
 
-console.log(`City: ${city.toUpperCase()}`);
-console.log(city.charAt(4));
-console.log(city.indexOf('a'));
+// console.log(`City: ${city.toUpperCase()}`);
+// console.log(city.charAt(4));
+// console.log(city.indexOf('a'));
+
+const username="  ashutosh8210 "
+
+console.log(username.trim());
+console.log(city.length);
+console.log(city.substring(1,5));
+console.log(city.slice(0,4));
+console.log(city.slice(0,-4));
+
+const url="https://www.ashutosh%20kumar.com/homepage%20profile"
+
+console.log(url.replace('%20','-'));
+console.log(url.replaceAll('%20','-'));
 
