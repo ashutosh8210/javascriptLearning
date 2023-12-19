b981102d684fcc979869bde4f7469645af167569
Ashutosh kumar <rajashutosh556@gmail.com>
2023-12-20 00:33:42 +0530

    strings basics

---
+++ 01_basics/01_variables/04_string.js
@@ -0,0 +1,13 @@
+const name="Ashutosh"
+const age="23"
+
+// console.log(name +" "+age)
+
+console.log(`My name is ${name} and my age is ${23}`);
+
+const city=new String("bangalore")
+
+console.log(`City: ${city.toUpperCase()}`);
+console.log(city.charAt(4));
+console.log(city.indexOf('a'));
+
