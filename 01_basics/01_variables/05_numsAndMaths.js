5bbf3554b2f13f1677350182b6fa69d49b3559e2
Ashutosh kumar <rajashutosh556@gmail.com>
2023-12-21 00:46:55 +0530

    numbers and maths

---
+++ 01_basics/01_variables/05_numsAndMaths.js
@@ -0,0 +1,33 @@
+const score=100
+console.log(score);
+
+const balance=new Number(426)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2))
+
+
+const number=542.873
+
+// console.log(number.toPrecision(3));
+// console.log(number.toPrecision(2));
+// console.log(number.toPrecision(4));
+
+const hundreds=10000000
+
+// console.log(hundreds.toLocaleString('en-US'));
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++Math+++++++
+
+console.log(Math.abs(-6))
+
+// console.log(Math.random())   //----> returns random number between 0 and 1
+// console.log((Math.random()*10)+1) 
+// console.log(Math.floor(Math.random()*10)) 
+
+
+const max=20
+const min=10
+
+console.log((Math.random()*(max-min)+1));
