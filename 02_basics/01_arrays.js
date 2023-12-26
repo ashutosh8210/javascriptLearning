1cf9dfbd6f48e3f0d14fef02252625a0a1d2cad7
Ashutosh kumar <rajashutosh556@gmail.com>
2023-12-26 17:51:18 +0530

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,4 +1,27 @@
-let myArray=[12,26,34,55]
+let myArray=[12,26,34,55,68,73]
 
 console.log(myArray);
-console.log(myArray[1])
+// console.log(myArray[1])
+
+// myArray.push(62)
+
+// console.log(myArray)
+
+// myArray.pop()
+
+// console.log(myArray);
+
+// unshift adds at start and shift remove from start
+
+// slice and splice
+
+let arr1=myArray.slice(1,3)
+console.log("A",arr1);
+
+console.log(myArray);
+
+let arr2=myArray.splice(1,3)
+console.log("B",arr2);
+console.log(myArray);
+
+// slice and splice both return arrays, but slice returns copy of array while splice make changes in original array.
